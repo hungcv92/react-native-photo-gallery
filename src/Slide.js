91dafef7ffa76f93b762320763e11dfc7e3d3c17
Andrés Espagnolo <andres@underscope.io>
2018-07-30 14:34:26 -0300

    Use PhotoView in slide in iOS and fix warning in carousel

--- src/Slide.js
+++ src/Slide.js
@@ -1,66 +1,47 @@
-import React, { Component } from 'react';
-import { Platform, Image, ActivityIndicator, Dimensions, View, ScrollView } from 'react-native';
-import PhotoView from 'react-native-photo-view';
+import React, { Component } from 'react'
+import { ActivityIndicator, Dimensions, View } from 'react-native'
+import PhotoView from 'react-native-photo-view'
+import PropTypes from 'prop-types'
+
+export class Slide extends Component {
+  render() {
+    const { item } = this.props
+
+    const inside = {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height - 128,
+    }
+
+    return (
+      <View
+        style={[styles.container, { width: Dimensions.get('window').width, height: Dimensions.get('window').height }]}
+      >
+        <ActivityIndicator style={styles.loader} />
+        <PhotoView source={item.image} maximumZoomScale={3} resizeMode="contain" style={[styles.image, inside]} />
+        {item.overlay}
+      </View>
+    )
+  }
+}
+
+Slide.propTypes = {
+  item: PropTypes.object,
+}
 
 const styles = {
-  slideC: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  scrollViewC: {
+  image: {
     alignItems: 'center',
-    top: Platform.OS === 'android' ? -32 : 70,
     justifyContent: 'center',
+    top: -32,
   },
   loader: {
     position: 'absolute',
-    top: (Dimensions.get('window').height / 2) - 10,
-    left: (Dimensions.get('window').width / 2) - 10,
+    top: Dimensions.get('window').height / 2 - 10,
+    left: Dimensions.get('window').width / 2 - 10,
   },
-};
-
-export class Slide extends Component {
-  render() {
-    const inside = {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height - 128,
-    };
-
-    return (
-      <View
-        style={[
-          styles.slideC,
-          { width: Dimensions.get('window').width, height: Dimensions.get('window').height }
-        ]}
-      >
-        <ActivityIndicator style={styles.loader} />
-        {Platform.OS === 'android' ?
-          <PhotoView
-            source={this.props.item.image}
-            maximumZoomScale={3}
-            zoomScale={1}
-            androidScaleType="center"
-            resizeMode="contain"
-            style={[
-              styles.scrollViewC,
-              inside
-            ]}
-          /> :
-          <ScrollView
-            maximumZoomScale={3}
-            zoomScale={1}
-            style={[{ flex: 1 }, inside]}
-            contentContainerStyle={[
-              styles.scrollViewC,
-              inside
-            ]}
-          >
-            <Image source={this.props.item.image} style={inside} resizeMode="contain" />
-          </ScrollView>
-        }
-        {this.props.item.overlay}
-      </View>
-    );
-  }
 }
